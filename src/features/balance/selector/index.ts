eaba8a048969e72d50e985accb942a3a642499d7
Martazina Anna <anqweta@gmail.com>
2026-08-07 16:33:30 +0300

    refactor: convert to monorepo with frontend/backend submodules

--- src/features/balance/selector/index.ts
+++
@@ -1,3 +0,0 @@
-import { RootState } from "@/features/store";
-
-export const moneySelector = (state: RootState) => state.balance.money;
--- src/types/typesBalance.ts
+++
@@ -1,7 +0,0 @@
-export type handleMoneyPayload = {
-    amount: number
-}    
-
-export type handleMoneyWinPayload = {
-    moneyWin: number;
-}    
